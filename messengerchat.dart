2c2f18d767899a4323079b8c0c38519200de33a3
Omar Anan <78151847+omar3anan@users.noreply.github.com>
2022-02-11 23:03:55 +0200

    Add files via upload

---
+++ messengerchat.dart
@@ -0,0 +1,236 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class MessengerScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        titleSpacing: 20.0,
+        title: Row(
+          children: const [
+            CircleAvatar(
+              radius: 20.0,
+              backgroundImage: NetworkImage(
+                  'https://simg.nicepng.com/png/small/289-2892199_admin-09-17-facebook-no-profile-pic-female.png'),
+            ),
+            SizedBox(
+              width: 15.0,
+            ),
+            Text(
+              'Chats',
+              style: TextStyle(
+                color: Colors.black,
+              ),
+            ),
+          ],
+        ),
+        actions: [
+          IconButton(
+            icon: const CircleAvatar(
+              radius: 15.0,
+              backgroundColor: Colors.blue,
+              child: Icon(
+                Icons.camera_alt,
+                size: 16.0,
+                color: Colors.white,
+              ),
+            ),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: CircleAvatar(
+              radius: 15.0,
+              backgroundColor: Colors.blue,
+              child: Icon(
+                Icons.edit,
+                size: 16.0,
+                color: Colors.white,
+              ),
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(
+                    5.0,
+                  ),
+                  color: Colors.grey[300],
+                ),
+                padding: const EdgeInsets.all(
+                  5.0,
+                ),
+                child: Row(
+                  children: const [
+                    Icon(
+                      Icons.search,
+                    ),
+                    SizedBox(
+                      width: 15.0,
+                    ),
+                    Text(
+                      'Search',
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Container(
+                height: 103.0,
+                child: ListView.separated(
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) => buildStoryItem(),
+                  separatorBuilder: (context, index) => SizedBox(
+                    width: 20.0,
+                  ),
+                  itemCount: 5,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              ListView.separated(
+                physics: NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemBuilder: (context, index) => buildChatItem(),
+                separatorBuilder: (context, index) => SizedBox(
+                  height: 20.0,
+                ),
+                itemCount: 15,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  // 1. build item
+  // 2. build list
+  // 3. add item to list
+
+  // arrow function
+  Widget buildChatItem() => Row(
+        children: [
+          Stack(
+            alignment: AlignmentDirectional.bottomEnd,
+            children: const [
+              CircleAvatar(
+                radius: 30.0,
+                backgroundImage: NetworkImage(
+                    'https://media-exp1.licdn.com/dms/image/C4E03AQGjSFMyevMP5A/profile-displayphoto-shrink_800_800/0/1632604510688?e=1639008000&v=beta&t=R0B_Ly9JRhIfmdQsaw0aatB-3dCtcn852Dau1j7F_qY'),
+              ),
+              Padding(
+                padding: EdgeInsetsDirectional.only(
+                  bottom: 3.0,
+                  end: 3.0,
+                ),
+                child: CircleAvatar(
+                  radius: 7.0,
+                  backgroundColor: Colors.green,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            width: 20.0,
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Omar Anan Abed Awad Mohammed Abou-Romia',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                SizedBox(
+                  height: 5.0,
+                ),
+                Row(
+                  children: [
+                    const Expanded(
+                      child: Text(
+                        'anan created a messenger screen using dart tell em your opinion',
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10.0,
+                      ),
+                      child: Container(
+                        width: 7.0,
+                        height: 7.0,
+                        decoration: BoxDecoration(
+                          color: Colors.blue,
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      '02:00 pm',
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      );
+
+  Widget buildStoryItem() => Container(
+        width: 60.0,
+        child: Column(
+          children: [
+            Stack(
+              alignment: AlignmentDirectional.bottomEnd,
+              children: const [
+                CircleAvatar(
+                  radius: 30.0,
+                  backgroundImage: NetworkImage(
+                      'https://media-exp1.licdn.com/dms/image/C4E03AQGjSFMyevMP5A/profile-displayphoto-shrink_800_800/0/1632604510688?e=1639008000&v=beta&t=R0B_Ly9JRhIfmdQsaw0aatB-3dCtcn852Dau1j7F_qY'),
+                ),
+                Padding(
+                  padding: EdgeInsetsDirectional.only(
+                    bottom: 3.0,
+                    end: 3.0,
+                  ),
+                  child: CircleAvatar(
+                    radius: 7.0,
+                    backgroundColor: Colors.green,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 6.0,
+            ),
+            const Text(
+              'Omar Anan Abed Awad ',
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ],
+        ),
+      );
+}
